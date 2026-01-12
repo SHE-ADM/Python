207d3c1f6fb4e4058eac43cb1d98430c8f4fa411
SHE-ADM <SHE-ADM@sheild.onmicrosoft.com>
2026-01-12 07:28:56 -0300

    Formato Data

--- FormatoData.py
+++ FormatoData.py
@@ -1,5 +1,6 @@
 
 from datetime import datetime, timezone
+import locale
 
 agora = datetime.now(timezone.utc)
 
@@ -11,4 +12,9 @@ print(f"{agora:%d/%m/%Y %H:%M}")       # 12/01/2026 06:56  (se convertido ao seu
 
 # Nome do mês e dia da semana
 print(f"{agora:%A, %d de %B de %Y}")   # Monday, 12 de January de 2026 (depende de locale)
-``
+
+# Formato Brasileiro
+locale.setlocale(locale.LC_TIME, 'pt_BR.UTF-8')
+print(f"{agora:%A, %d de %B de %Y}")   # segunda-feira, 12 de janeiro de 2026
+# Formato com milissegundos
+print(f"{agora:%Y-%m-%d %H:%M:%S.%f}")  # 2026-01-12 09:56:55.123456
