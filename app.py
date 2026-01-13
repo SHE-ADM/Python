43b73fdc516a4ffdbe5caec1e069847d496de978
SHE-ADM <SHE-ADM@sheild.onmicrosoft.com>
2026-01-13 06:03:09 -0300

    if elif else

--- app.py
+++ app.py
@@ -15,4 +15,17 @@ print('3. Ativar Restaurante')
 print('4. Sair\n')
 
 opcao_escolhida = input('Escolha uma opção: ')
-print(f'você escolheu a opção: {opcao_escolhida}')
+opcao_escolhida = int(opcao_escolhida)    
+
+if opcao_escolhida == 1:
+   print('Cadastrar Restaurante')
+
+elif opcao_escolhida == 2:
+   print('Listar Restaurantes')
+     
+elif opcao_escolhida == 3:
+   print('Ativar Restaurante')
+
+else:
+   print('Sair do Programa')
+   
