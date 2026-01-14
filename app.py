c41388ae8a7ea281c2f69d87a7facc56fec59bd1
SHE-ADM <SHE-ADM@sheild.onmicrosoft.com>
2026-01-14 06:12:58 -0300

    Trocando if por match

--- app.py
+++ app.py
@@ -22,7 +22,7 @@ def fim_app():
     os.system('cls')      
     print('\nObrigado por usar o sistema de restaurantes!\n')     
 
-def escolher_opcao():   
+def escolher_opcao_usando_if():   
    opcao_escolhida = input('Escolha uma opção: ')
    opcao_escolhida = int(opcao_escolhida)    
 
@@ -37,12 +37,28 @@ def escolher_opcao():
 
    else:
          fim_app()
-      
+         
+def escolher_opcao_usando_match():   
+   opcao_escolhida = input('Escolha uma opção: ')
+   opcao_escolhida = int(opcao_escolhida)    
+
+   match opcao_escolhida:
+      case 1:
+         print('Cadastrar Restaurante')
+
+      case 2:
+         print('Listar Restaurantes')
+         
+      case 3:
+         print('Ativar Restaurante')
+
+      case _:
+         fim_app()      
     
 def main():
     exibir_nome() 
     exibir_opcoes()
-    escolher_opcao()
+    escolher_opcao_usando_match()
         
 if __name__ == '__main__':
     main()
