07dabb282483aebcccb9297a099b75bc8009302c
SHE-ADM <SHE-ADM@sheild.onmicrosoft.com>
2026-01-14 06:07:51 -0300

    Aprofundamento em funções

--- app.py
+++ app.py
@@ -1,9 +1,8 @@
 import os
 
-
-
-print("""      
-      
+def exibir_nome():
+   print("""
+                  
 ░██████╗░█████╗░██████╗░░█████╗░██████╗░  ███████╗██╗░░██╗██████╗░██████╗░███████╗░██████╗░██████╗
 ██╔════╝██╔══██╗██╔══██╗██╔══██╗██╔══██╗  ██╔════╝╚██╗██╔╝██╔══██╗██╔══██╗██╔════╝██╔════╝██╔════╝
 ╚█████╗░███████║██████╦╝██║░░██║██████╔╝  █████╗░░░╚███╔╝░██████╔╝██████╔╝█████╗░░╚█████╗░╚█████╗░
@@ -13,29 +12,39 @@ print("""
       
       """)
 
-print('1. Cadastrar Restaurante')
-print('2. Listar Restaurantes')
-print('3. Ativar Restaurante')
-print('4. Sair\n')
-
-opcao_escolhida = input('Escolha uma opção: ')
-opcao_escolhida = int(opcao_escolhida)    
-
+def exibir_opcoes():
+    print('1. Cadastrar Restaurante')
+    print('2. Listar Restaurantes')
+    print('3. Ativar Restaurante')
+    print('4. Sair\n')
+    
 def fim_app():
     os.system('cls')      
     print('\nObrigado por usar o sistema de restaurantes!\n')     
 
+def escolher_opcao():   
+   opcao_escolhida = input('Escolha uma opção: ')
+   opcao_escolhida = int(opcao_escolhida)    
 
+   if opcao_escolhida == 1:
+      print('Cadastrar Restaurante')
 
-if opcao_escolhida == 1:
-   print('Cadastrar Restaurante')
+   elif opcao_escolhida == 2:
+      print('Listar Restaurantes')
+      
+   elif opcao_escolhida == 3:
+      print('Ativar Restaurante')
 
-elif opcao_escolhida == 2:
-   print('Listar Restaurantes')
-     
-elif opcao_escolhida == 3:
-   print('Ativar Restaurante')
-
-else:
-      fim_app()
-   
+   else:
+         fim_app()
+      
+    
+def main():
+    exibir_nome() 
+    exibir_opcoes()
+    escolher_opcao()
+        
+if __name__ == '__main__':
+    main()
+    
+    
