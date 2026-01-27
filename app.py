6b481a473f31ea877b38d78994ac6dc613d0101e
SHE-ADM <SHE-ADM@sheild.onmicrosoft.com>
2026-01-27 07:09:53 -0300

    Dicionários

--- app.py
+++ app.py
@@ -1,7 +1,10 @@
 import os
 from tracemalloc import start
 
-restaurantes = []
+restaurantes = [{'nome': 'Praça1', 'categoria': 'Comida japonesa', 'ativo': False},
+                {'nome': 'Praça2', 'categoria': 'Comida japonesa', 'ativo': False},
+                {'nome': 'Praça3', 'categoria': 'Comida japonesa', 'ativo': False}]
+
 
 
 def exibir_nome():
@@ -28,16 +31,12 @@ def exibir_subtitulos(texto):
    print()
        
 def voltar_menu_principal():
-    input('Digite Enter para voltar ao menu principal...')
+    input('Digite Enter para voltar ao menu principal... ')
     main()
 
 def opcao_invalida():
-    exibir_subtitulos('\nOpção inválida! Tente novamente.\n')
+    exibir_subtitulos('Opção inválida! Tente novamente.')
     voltar_menu_principal()
-
-
-def fim_app():
-    exibir_subtitulos('\nObrigado por usar o sistema de restaurantes!\n')        
    
 def cadadastrar_restaurante(): 
    exibir_subtitulos('Cadastro de novos restaurantes')
@@ -45,17 +44,21 @@ def cadadastrar_restaurante():
    restaurantes.append(nome_restaurante)
    print(f'\nRestaurante "{nome_restaurante}" cadastrado com sucesso!\n')
    voltar_menu_principal()
-   
-   
+      
 def listar_restaurante():
    exibir_subtitulos('Listagem de Restaurantes')
 
    for restaurante in restaurantes:
-      print(f'{restaurante}')
-      print()  
-      voltar_menu_principal()
-   
-   
+      nome_restaurante = restaurante['nome']
+      categoria = restaurante['categoria']
+      ativo = restaurante['ativo']
+      print(f' - {nome_restaurante} | Categoria: {categoria} | Ativo: {ativo}')
+    
+   voltar_menu_principal()
+  
+def fim_app():
+    exibir_subtitulos('Obrigado por usar o sistema de restaurantes!')        
+ 
 def escolher_opcao_usando_if():  
    try:
       opcao_escolhida = input('Escolha uma opção: ')
@@ -78,25 +81,6 @@ def escolher_opcao_usando_if():
    except ValueError:
       opcao_invalida()  
          
-
-def escolher_opcao_usando_match():   
-   opcao_escolhida = input('Escolha uma opção: ')
-   opcao_escolhida = int(opcao_escolhida)    
-
-   match opcao_escolhida:
-      case 1:
-         print('Cadastrar Restaurante')
-
-      case 2:
-         print('Listar Restaurantes')
-         
-      case 3:
-         print('Ativar Restaurante')
-
-      case _:
-         fim_app()      
-        
-
 def main():
     exibir_nome() 
     exibir_opcoes()
